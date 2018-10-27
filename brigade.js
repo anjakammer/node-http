f0db654116cb16e4d339531d2231cea2140cf654
anjakammer <post@anjakammer.de>
2018-10-27 21:44:01 +0200

    debug promise error

--- brigade.js
+++ brigade.js
@@ -11,14 +11,14 @@ events.on('check_run:rerequested', checkRequested)
 function checkRequested (e, p) {
   console.log('Check-Suite requested')
 
-  registerCheckRun(e.payload)
+  registerCheckSuite(e.payload)
     .then(() => { return runCheckSuite(e.payload) })
     .then(() => { return console.log('Finished Check-Suite') })
     .catch((err) => { console.log(err) })
 }
 
-function registerCheckRun (payload) {
-  return eachSeries(stages, (check, next) => {
+function registerCheckSuite (payload) {
+  eachSeries(stages, (check, next) => {
     console.log(`register-${check}`)
 
     const registerCheck = new Job(`register-${check}`.toLocaleLowerCase(), checkRunImage)
@@ -34,6 +34,7 @@ function registerCheckRun (payload) {
       .then(() => { return next() })
       .catch(err => { console.log(err) })
   })
+  return Promise.resolve('Finished Check-Suite registration')
 }
 
 function runCheckSuite (payload) {
@@ -41,29 +42,29 @@ function runCheckSuite (payload) {
     console.log(`run-${check}`)
     const runCheck = new Job(check.toLocaleLowerCase(), 'alpine:3.7', ['sleep 60', 'echo hello'])
 
-    const end = new Job(`assert-result-of-${check}-job`.toLocaleLowerCase(), checkRunImage)
-    end.imageForcePull = true
-    end.env = {
+    const assertResult = new Job(`assert-result-of-${check}-job`.toLocaleLowerCase(), checkRunImage)
+    assertResult.imageForcePull = true
+    assertResult.env = {
       CHECK_PAYLOAD: payload,
       CHECK_NAME: check,
       CHECK_TITLE: 'Description'
     }
     return runCheck.run()
       .then((result) => {
-        end.env.CHECK_CONCLUSION = 'success'
-        end.env.CHECK_SUMMARY = `Job:${check} completed`
-        end.env.CHECK_TEXT = result.toString() + 'where am I?'
-        return end.run()
+        assertResult.env.CHECK_CONCLUSION = 'success'
+        assertResult.env.CHECK_SUMMARY = `Job:${check} completed`
+        assertResult.env.CHECK_TEXT = result.toString() + 'where am I?'
+        return assertResult.run()
           .then(() => { return next() })
           .catch(err => { console.log(err) })
       })
       .catch((err) => {
-        end.env.CHECK_CONCLUSION = 'failed'
-        end.env.CHECK_SUMMARY = `Job:${check} failed`
-        end.env.CHECK_TEXT = `Error: ${err}`
-        return end.run()
+        assertResult.env.CHECK_CONCLUSION = 'failed'
+        assertResult.env.CHECK_SUMMARY = `Job:${check} failed`
+        assertResult.env.CHECK_TEXT = `Error: ${err}`
+        return assertResult.run()
       })
   })
 }
 
-module.exports = {registerCheckRun, runCheckSuite}
+module.exports = {registerCheckSuite, runCheckSuite}
