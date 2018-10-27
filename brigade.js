180575348f258ae409a520a9dc8a6f6767ea9536
anjakammer <post@anjakammer.de>
2018-10-27 20:04:06 +0200

    dummy prototype WIP

--- brigade.js
+++ brigade.js
@@ -1,51 +1,68 @@
 const {events, Job} = require('brigadier')
+const eachSeries = require('async/eachSeries')
+
 const checkRunImage = 'technosophos/brigade-github-check-run:latest'
+const stages = ['Build', 'Test', 'Deploy']
 
-events.on('check_suite:requested', checkRequested)
-events.on('check_suite:rerequested', checkRequested)
-events.on('check_run:rerequested', checkRequested)
+events.on('check_suite:requested', this.checkRequested)
+events.on('check_suite:rerequested', this.checkRequested)
+events.on('check_run:rerequested', this.checkRequested)
 
-function checkRequested (e, p) {
-  console.log('check requested')
-  // Common configuration
-  const env = {
-    CHECK_PAYLOAD: e.payload,
-    CHECK_NAME: 'MyService',
-    CHECK_TITLE: 'Echo Test'
-  }
+exports.checkRequested = (e, p) => {
+  console.log('Check-Suite requested')
 
-  // This will represent our build job. For us, it's just an empty thinger.
-  const build = new Job('build', 'alpine:3.7', ['sleep 60', 'echo hello'])
-
-  // For convenience, we'll create three jobs: one for each GitHub Check
-  // stage.
-  const start = new Job('start-run', checkRunImage)
-  start.imageForcePull = true
-  start.env = env
-  start.env.CHECK_SUMMARY = 'Beginning test run'
-
-  const end = new Job('end-run', checkRunImage)
-  end.imageForcePull = true
-  end.env = env
-
-  // Now we run the jobs in order:
-  // - Notify GitHub of start
-  // - Run the test
-  // - Notify GitHub of completion
-  //
-  // On error, we catch the error and notify GitHub of a failure.
-  start.run().then(() => {
-    return build.run()
-  }).then((result) => {
-    end.env.CHECK_CONCLUSION = 'success'
-    end.env.CHECK_SUMMARY = 'Build completed'
-    end.env.CHECK_TEXT = result.toString()
-    return end.run()
+  this.registerCheckRun(e.payload).then(() => {
+    return this.runCheck(e.payload)
+  }).then(() => {
+    console.log('Finished Check-Suite')
   }).catch((err) => {
-    // In this case, we mark the ending failed.
-    end.env.CHECK_CONCLUSION = 'failed'
-    end.env.CHECK_SUMMARY = 'Build failed'
-    end.env.CHECK_TEXT = `Error: ${err}`
-    return end.run()
+    console.log(err)
+  })
+}
+
+exports.registerCheckRun = (payload) => {
+  eachSeries(stages, (check, next) => {
+    console.log(`register-${check}`)
+
+    const registerCheck = new Job(`register-${check}`, checkRunImage)
+    registerCheck.imageForcePull = true
+    registerCheck.env = {
+      CHECK_PAYLOAD: payload,
+      CHECK_NAME: check,
+      CHECK_TITLE: 'Description'
+    }
+    registerCheck.env.CHECK_SUMMARY = `${check} scheduled`
+
+    return registerCheck.run().then((result) => {
+      console.log(result.toString())
+      next()
+    })
+  })
+}
+
+exports.runCheck = (payload) => {
+  eachSeries(stages, (check, next) => {
+    console.log(`run-${check}`)
+    const runCheck = new Job(check, 'alpine:3.7', ['sleep 60', 'echo hello'])
+
+    const end = new Job(`assert-result-of-${check}`, checkRunImage)
+    end.imageForcePull = true
+    end.env = {
+      CHECK_PAYLOAD: payload,
+      CHECK_NAME: check,
+      CHECK_TITLE: 'Description'
+    }
+    return runCheck.run().then((result) => {
+      end.env.CHECK_CONCLUSION = 'success'
+      end.env.CHECK_SUMMARY = `${check} completed`
+      end.env.CHECK_TEXT = result.toString() + 'where am I?'
+      end.run()
+      next()
+    }).catch((err) => {
+      end.env.CHECK_CONCLUSION = 'failed'
+      end.env.CHECK_SUMMARY = `${check} failed`
+      end.env.CHECK_TEXT = `Error: ${err}`
+      return end.run()
+    })
   })
 }
