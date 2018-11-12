308777f460af3bc5cd425c80ebc2d7528bb8e122
Anja Kammer <post@anjakammer.de>
2018-11-12 19:58:48 +0100

    WIP deploy service and ingress

--- brigade.js
+++ brigade.js
@@ -83,10 +83,10 @@ async function runCheckSuite (payload, secrets) {
   deploy.serviceAccount = 'anya-deployer'
   deploy.tasks = [
     `echo "Deploying ${appName}:${imageTag}"`,
-    `kubectl run ${appName}-${imageTag}-preview --image=${imageName} --labels="app=${appName}" --port=80 -n preview`,
+    `kubectl run ${appName}-${imageTag}-preview --image=${imageName} --labels="app=${appName}-${imageTag}-preview" --port=80 -n preview`,
     'cd /src/manifest',
     `sed -i -e 's/previewPath/${imageTag}/g' -e 's/app-name/${appName}/g' ingress.yaml`,
-    `sed -i -e 's/app-name/${appName}/g' service.yaml`,
+    `sed -i -e 's/name: app-name/name: ${appName}/g' -e 's/appName-imageTag-preview/${appName}-${imageTag}-preview/g' service.yaml`,
     'kubectl apply -f service.yaml -n preview',
     'kubectl apply -f ingress.yaml -n preview',
     `echo "Status of ${appName}:${imageTag}:"`,
