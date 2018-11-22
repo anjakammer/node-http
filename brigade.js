4361fb3a576915bfe4efd935f37ed87049d657f1
Anja Kammer <post@anjakammer.de>
2018-11-22 02:21:44 +0100

    re-request check suite in first pr commit

--- brigade.js
+++ brigade.js
@@ -21,9 +21,9 @@ function checkRequested (e, p) {
   if (pr.length === 0) {
     // re-request the check, to get the pr-id
     console.log('No PR-id found. Will re-request the check_suite')
-    console.log(webhook.check_suite.pull_requests.url)
+    console.log(webhook.check_suite.url)
     request({
-      uri: webhook.check_suite.pull_requests.url,
+      uri: webhook.check_suite.url,
       json: true,
       headers: {
         'Authorization': 'token ' + JSON.parse(e.payload).token,
